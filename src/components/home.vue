213fe00a4750ad3d20def3ac7c0188f415c2cfcd
Alice <18692929437@163.com>
2025-08-05 23:04:38 +0800

    开发表格
    Signed-off-by: Alice <18692929437@163.com>

---
+++ src/components/home.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const collectDatas = ref([])
+</script>
+
+<template>
+    <div class="home-views">
+        <div class="title">
+            <img src="/logo.png" width="160px" height="56px" alt="">
+        </div>
+      <el-table :data="collectDatas" style="width: 100%">
+          <el-table-column prop="date" label="Date" width="180" />
+          <el-table-column prop="name" label="Name" width="180" />
+          <el-table-column prop="address" label="Address" />
+        </el-table>
+    </div>
+</template>
+
+<style scoped>
+.home-views{
+    height: 100%;
+    width: 100%;
+    padding: 6px 16px 20px;
+    .title{
+        border-bottom: 1px solid #dfdfdf;
+        padding-bottom: 6px;
+        img{
+            display: block;
+        }
+        margin-bottom: 6px;
+    }
+}
+</style>
