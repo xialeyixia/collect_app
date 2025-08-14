69d6740184a2daf8cec7276be4a96eb6670c1690
Alice <18692929437@163.com>
2025-08-14 21:30:28 +0800

    修改端口
    Signed-off-by: Alice <18692929437@163.com>

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     server: {
+        port: 39997,
+        host: "0.0.0.0",
         proxy: {
             // 选项写法
             '/api': {
